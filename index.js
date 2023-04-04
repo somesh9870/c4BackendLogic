0ae73d65a109d92c5d836733cde31afdd8068220
somesh9870 <someshrawat106.com>
2023-04-04 14:18:22 +0530

    calculator route

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const connection = require("./db");
 const userRouter = require("./routes/user.routes");
 const cors = require("cors");
-const auth = require("./middlewares/auth.middleware");
-const postRouter = require("./routes/post.routes");
+// const auth = require("./middlewares/auth.middleware");
+const calculateRoute = require("./routes/calculator.routes");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/users", userRouter);
-app.use(auth);
-app.use("/posts", postRouter);
+// app.use(auth);
+// app.use("/posts", postRouter);
+
+app.use("/calculte", calculateRoute);
 
 app.listen(process.env.port, async () => {
   try {
